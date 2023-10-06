12f42ae0af0500d33838935e0772a3600b490f4b
AKIRA-0512 <shima0512akira@gmail.com>
2023-10-06 22:48:37 +0900

    カリキュラム8-4

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function() {
     return view('posts.index');
 });
 Route::get('/',  [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
